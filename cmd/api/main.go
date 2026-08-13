b6214002e7363e85808cc3b67e3c2a7343445753
sujay pradhan <135443280+sujay090@users.noreply.github.com>
2026-08-13 10:47:35 +0530

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+	"true_shop/internal/config"
 )
 
 
 
 func main(){
+	cfg := config.MustLoad()
+	fmt.Println("Server starting ")
 	mux := http.NewServeMux()
-	
 	mux.HandleFunc("GET /healthz",func(w http.ResponseWriter,r *http.Request){
 		w.Header().Set("Content-Type","application/json")
 		w.WriteHeader(http.StatusOK)
@@ -18,14 +21,14 @@ func main(){
 	})
 
 	srv := http.Server{
-		Addr: ":8090",
+		Addr: ":" + cfg.Port,
 		Handler: mux,
 		ReadTimeout: time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout: time.Second * 60,
 
 	}
-
+	log.Printf("Server listening %s",srv.Addr)
 	if err := srv.ListenAndServe();err !=nil {
 		log.Fatalf("server failed : %v",err)
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct{
+	Port string
+	Env string
+}
+
+func MustLoad() Config{
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == ""{
+		panic("PORT is required")
+	}
+	env := os.Getenv("ENV")
+	if env == ""{
+		panic("ENV is required")
+	}
+	return Config{
+		Port: port,
+		Env: env,
+	}
+}
